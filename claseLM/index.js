2a4584b477a215bf3cbcec215a7f5b73dc565e21
IsmaDevBalmis <75784444+IsmaDevBalmis@users.noreply.github.com>
2021-05-13 12:25:55 +0200

    1

--- claseLM/index.js
+++ claseLM/index.js
@@ -11,6 +11,13 @@ function gestionarFicheroXML(xml){
 	for(let i=0; i<libros.length; i++)
 		//capaVacia.innerHTML += "<div class='fila'>" + libros[i].textContent + "</div>"
 		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("ISBN").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("titulo").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("nivelProfundidad").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("autores").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("editorial").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("fechaPublicacion").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("paginaWeb").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("precio").textContent + "</div>"
 
 	
     
