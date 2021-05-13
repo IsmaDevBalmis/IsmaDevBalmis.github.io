74bfbda6bdc54780b6db1a0284f1245494d27e87
IsmaDevBalmis <75784444+IsmaDevBalmis@users.noreply.github.com>
2021-05-13 12:21:09 +0200

    1

--- claseLM/index.js
+++ claseLM/index.js
@@ -9,7 +9,9 @@ function gestionarFicheroXML(xml){
     let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xml.querySelectorAll("libro")
 	for(let i=0; i<libros.length; i++)
-		capaVacia.innerHTML += "<div class='fila'>" + libros[i].textContent + "</div>"
+		//capaVacia.innerHTML += "<div class='fila'>" + libros[i].textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("ISBN").textContent + "</div>"
+
 	
     
     let capa = document.querySelector("div:nth-child(1)") 
