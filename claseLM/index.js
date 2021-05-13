ab7a08ef05f54868638d9bb1126c9b87cd67d689
IsmaDevBalmis <75784444+IsmaDevBalmis@users.noreply.github.com>
2021-05-13 11:58:58 +0200

    1

--- claseLM/index.js
+++ claseLM/index.js
@@ -6,6 +6,15 @@ loadLDocA(fichero,"xml")
 
 function gestionarFicheroXML(xml){
 
-    alert(xml)
+    let capaVacia = document.querySelector("#ficheroXML")
+	let libros = xml.querySelectorAll("libro")
+	for(let i=0; i<libros.length; i++)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
+	
+    
+    let capa = document.querySelector("div:nth-child(1)") 
+    capa.addEventListener("click",CargarFichero);
+
 
 }
+
