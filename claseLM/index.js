ddad385e1edeadf0bde10ec1d2ed3e5fbfbc60e3
IsmaDevBalmis <75784444+IsmaDevBalmis@users.noreply.github.com>
2021-05-17 12:35:14 +0200

    1

--- claseLM/index.js
+++ claseLM/index.js
@@ -14,4 +14,17 @@ function gestionarFicheroTXT(documento){
 
    alert(lineas)
 
+
+   let capaVacia = document.querySelector("#ficheroTXT")
+
+   let chat = documento.querySelectorAll("mensaje")
+
+
+   for(let i=0; i<chat.length; i++){
+       //capaVacia.innerHTML += "<div class='fila'>" + libros[i].textContent + "</div>"
+       capaVacia.innerHTML += "<div class='celda'>" + lineas[i].textContent + "</div>"
+      
+   }
+
+
 }
